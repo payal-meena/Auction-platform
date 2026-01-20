ad51d9559759ba5cda1088019132c3417c5cd34c
shivanimeena11 <shivanimeena35689@gmail.com>
2026-01-20 13:47:35 +0530

    design message page

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/public/Home";
 import Dashboard from "../pages/dashboard/Dashboard";
+import MessagesPage from "../pages/messages/MessagesPage";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/messages" element={<MessagesPage />} />
     </Routes>
   );
 }
