3d09fd5e4708e7d5d92b0b68baa4c7a4d9a192d6
shivanimeena11 <shivanimeena35689@gmail.com>
2026-01-20 13:53:53 +0530

    add route

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -4,6 +4,7 @@ import Dashboard from "../pages/dashboard/Dashboard";
 import MySkills from "../pages/skills/MySkills";
 import UserLayout from "../layouts/UserLayout";
 import Requests from "../pages/requests/Requests";
+import MessagesPage from "../pages/messages/MessagesPage";
 
 function AppRoutes() {
   return (
@@ -14,6 +15,8 @@ function AppRoutes() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/my-skills" element={<MySkills />} />
         <Route path="/requests" element={<Requests/>} />
+        <Route path="/messages" element={< MessagesPage/>} />
+        
       </Route>
 
       <Route path="/admin" element={<div>Admin Layout Loading...</div>}>
